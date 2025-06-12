6124063b36bed982b4f48130ade7a31d43ca608a
Rahish4u <rahishali0591@gmail.com>
2025-06-12 13:12:51 +0530

    Day-2

---
+++ Day-2/index.js
@@ -0,0 +1,25 @@
+
+// Q-1: Write a program to check if a person is eligible to vote or not. A person is eligible to vote if they are 18 years or older.
+// var ans = prompt("enter youe name") 
+// if(isNaN(ans)) {
+//     console.log("Please enter a valid number");
+// }
+// else if (ans >= 18) {
+//     console.log("You are eligible to vote");  
+// }else {
+//     console.log("You are not eligible to vote");
+// }   
+
+// Q-2: Write a program to check if a number is even or odd.
+// let amount = Number (prompt("What is the final amount?"));
+// let discount = 0;
+// if (amount> 0 && amount <= 5000){
+//     discount = 0;
+// }else if (amount > 5000 && amount <= 7000){
+//     discount = 5;
+// }else if (amount > 7000 && amount <= 9000){
+//     discount = 10;
+// }else if (amount > 9000){
+//     discount = 20;  
+// }
+// console.log(amount - Math.floor ((discount * amount) / 100));
