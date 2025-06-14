77c281192bd976ff9a34feffbb2c185e0e15bd2f
Rahish4u <rahishali0591@gmail.com>
2025-06-14 17:33:34 +0530

    new pleoblem solved

--- Day-2/index.js
+++ Day-2/index.js
@@ -23,3 +23,20 @@
 //     discount = 20;  
 // }
 // console.log(amount - Math.floor ((discount * amount) / 100));
+
+let unit = Number(prompt("Enter the number of units consumed:"));
+let amount = 0;
+if (unit > 400){
+    amount = (unit - 400) * 13;
+    unit = 400; 
+}
+if (unit > 200 && unit <= 400){
+    amount += (unit - 200) * 10; 
+    unit = 200;
+}
+if (unit > 100 && unit <= 200){
+    amount += (unit - 100) * 8;
+    unit = 100;
+}
+amount += unit * 5;
+console.log("Total amount to be paid: " + amount + " Rs.");
