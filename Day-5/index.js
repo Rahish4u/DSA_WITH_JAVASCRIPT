c207e651e74444e14284da630bd5ec28021c1ae3
Rahish4u <rahishali0591@gmail.com>
2025-06-19 12:13:11 +0530

    Day-5 Array Problem

---
+++ Day-5/index.js
@@ -0,0 +1,49 @@
+// let arr= [];
+// arr.push(1);
+// arr.push(2);        
+// arr.push(3);
+// arr.push(4);
+// arr.push(5);
+// arr.push(6);
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+// arr.shift();
+// console.log(arr);
+// arr.unshift("Rahish");
+// console.log(arr);
+
+// sum of array elements
+// let arr = [1, 2, 3, 4, 5];
+// let sum = 0;
+// for(let i = 0; i < arr.length; i++){
+//     sum = sum + arr[i];
+// }
+// console.log("Sum of array elements is: " + sum);
+
+// Print maximum element in an array
+// let arr = [10, 20, 30, 40, 600, 100, 50];
+// let max = arr[0];
+// for (let i =1; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//         max = arr[i];
+//     }
+// }
+// console.log("Maximum element in the array is: " + max);
+
+// Print min element in an array
+// let min = arr[0];
+// for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] < min) {
+//         min = arr[i];
+//     }
+// }   
+// console.log("Minimum element in the array is: " + min);
+
+// print reverse of an array
+// let reversedArr = [];
+// for (let i = arr.length - 1; i >= 0; i--) {
+//     reversedArr.push(arr[i]);
+// }
+// console.log("Reversed array is: " + reversedArr);
+
