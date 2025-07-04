eedaa8d30dd84cb2cb78f75221171f95c384a226
Rahish4u <rahishali0591@gmail.com>
2025-07-04 11:29:07 +0530

    delete unesassary files

--- Day-8/index.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.getElementById("menuToggle");
-const navList = document.querySelector(".nav-list");
-
-menuToggle.addEventListener("click", () => {
-  navList.classList.toggle("active");
-});
