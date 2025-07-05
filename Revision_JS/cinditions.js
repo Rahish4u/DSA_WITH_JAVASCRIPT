74330a8353bc2b61fc0967730c392a9ab067a302
Rahish4u <rahishali0591@gmail.com>
2025-07-05 12:24:58 +0530

    Today i have revised coditions and solve some questions

---
+++ Revision_JS/cinditions.js
@@ -0,0 +1,89 @@
+// 1. Student grade logic
+// Write a program that prints A, B, C, D, or F based on marks
+
+// let marks = 80;
+// if (marks >90) {
+//     console.log("A");
+// }else if(marks >=80) {
+//     console.log("B");
+// }else if (marks >=70) {
+//     console.log("C");
+// }else if (marks >=60) {
+//     console.log("D");
+// }else {
+//     console.log("F");
+// }
+
+// Function to determine grade based on marks
+// function grade(marks) {
+//   if (marks >= 90) { return "A";}
+//     else if (marks >= 80) { return "B";}
+//     else if (marks >= 70) { return "C";}
+//     else if (marks >= 60) { return "D";}
+//     else { return "F";}
+// }
+// console.log(grade(95)); 
+
+
+// 2. Rock-paper-scissors game
+// Given player1 and player2's choice, print winner or draw
+
+// function rockPaperScissors(player1, player2) {
+//     if (player1 === player2) {   
+//         return "It's a draw!";   
+//     } else if ((player1 === "rock" && player2 === "scissors") ||
+//                (player1 === "scissors" && player2 === "paper") ||
+//                (player1 === "paper" && player2 === "rock")) {
+//         return "Player 1 wins!";
+//     } else {
+//         return "Player 2 wins!";
+//     }
+// }
+// console.log(rockPaperScissors("rock", "scissors"));
+
+// 3. Login message:
+// let isLoggedIn = true;
+// let isAdmin = false;
+
+// if (isLoggedIn && isAdmin) {
+//     console.log("Welcome Admin!");
+// }
+// else if (isLoggedIn) {
+//     console.log("Welcome User!");
+// } else {
+//     console.log("Please log in.");
+// }
+
+// 4. Weather advice:
+// Weather advice:
+
+// let weather = "rainy"; // Change this to test different weather conditions
+// switch (weather){
+//     case "sunny" : 
+//         console.log("It's sunny, wear sunglasses!");
+//         break;
+//     case "cloudy" :
+//         console.log("It's cloudy, might rain later.");
+//         break;
+//     case "rainy" :
+//         console.log("It's rainy, take an umbrella!");
+//         break;
+//     case "snowy" :
+//         console.log("It's snowy, wear a warm coat!");
+//         break;
+//     default:
+//         console.log("Weather condition unknown, dress accordingly!");   
+// }
+
+// 5. Age Checker
+
+// let ageCheck = 88;
+// if (ageCheck <13) {
+//     console.log("You are a child.");
+// } else if (ageCheck >= 13 && ageCheck < 20) {
+//     console.log("You are a teenager.");
+// } else if (ageCheck >= 20 && ageCheck < 60) {
+//     console.log("You are an adult.");
+// } else {
+//     console.log("You are a senior citizen.");
+// }
